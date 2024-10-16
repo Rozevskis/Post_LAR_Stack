1dca5317e1c2424fab1d804f597aaba2288c3f67
Kristofers Jēkabs Rozevskis <Kristofers.rozevskis@gmail.com>
2024-10-16 10:54:19 +0300

    Register function incomplete 13:27 of https://www.youtube.com/watch?v=Qfe7QlFFArA

--- react-frontend/src/Pages/Auth/Register.jsx
+++ react-frontend/src/Pages/Auth/Register.jsx
@@ -1,7 +1,36 @@
 export default function Register(){
+
+    cont [FormData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+    })
+    function handleRegister(e){
+        e.preventDefault();
+        console.log(FormData);
+    }
+
     return(
         <>
         <h1 className="title">Register a new account</h1>
+
+        <form onSubmit={handleRegister} className="w-1/2 mx-auto space-y-6">
+            <div>
+                <input type="text" name="" placeholder="Name" />
+            </div>
+            <div>
+                <input type="text" name="" placeholder="Email" />
+            </div>
+            <div>
+                <input type="password" name="" placeholder="Password" />
+            </div>
+            <div>
+                <input type="password" name="" placeholder="Password Confirmation" />
+            </div>
+            <button className="primary-btn">Register</button>
+
+        </form>
         </>
     );
 }
