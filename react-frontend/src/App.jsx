4a23a69ab5e8a840ac147bdd79e8168c4daa9621
Kristofers Jēkabs Rozevskis <Kristofers.rozevskis@gmail.com>
2024-10-16 10:46:18 +0300

    basic Auth pages and nav links

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
 
 export default function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout/>}>
           <Route index element={<Home/>}/>
+          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
       </Route>
+     
     </Routes>
   </BrowserRouter>;
 }
