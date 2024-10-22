43c8d61b2b2b43d7e79747e9f57d7fa84bc98d93
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-10-22 14:04:54 +0300

    Fixed minnor error on api error response

--- laravel-api-backend/app/Http/Controllers/AuthController.php
+++ laravel-api-backend/app/Http/Controllers/AuthController.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $fields = $request->validate([
             'name' => 'required|max:255',
             'email' => 'required|email|unique:users',
@@ -18,14 +19,15 @@ class AuthController extends Controller
         $user = User::create($fields);
 
         $token = $user->createToken($request->name);
-        $plainTextToken = $token->plainTextToken;   
+        $plainTextToken = $token->plainTextToken;
         return [
             'user' => $user,
             'token' => $token->plainTextToken
         ];
     }
 
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $request->validate([
             'email' => 'required|email|exists:users',
             'password' => 'required'
@@ -34,18 +36,23 @@ class AuthController extends Controller
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['message' => 'The provided credentials are incorrect.'], 401);
+            return [
+                'errors' => [
+                    'email' => ['The provided credentials are incorrect.']
+                ]
+            ];
         }
 
         $token = $user->createToken($user->name);
-        
+
         return [
             'user' => $user,
             'token' => $token->plainTextToken
         ];
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         $request->user()->tokens()->delete();
 
         return [
