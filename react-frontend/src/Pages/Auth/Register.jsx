b75ec5552e0582041c39285c531ff1d0b7aed28a
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-10-22 10:40:12 +0300

    Fixed error in vite config and register returns response in console

--- react-frontend/src/Pages/Auth/Register.jsx
+++ react-frontend/src/Pages/Auth/Register.jsx
@@ -6,11 +6,21 @@ export default function Register(){
         email: '',
         password: '',
         password_confirmation: '',
-    })
-    function handleRegister(e){
+    });
+
+
+
+    async function handleRegister(e){
         e.preventDefault();
         console.log(FormData);
+        const res = await fetch('/api/register', {
+            method: 'POST',
+            body: JSON.stringify(FormData),
+        });
+        const data = await res.json();
+        console.log(data);
     }
+    
 
     return(
         <>
@@ -18,18 +28,22 @@ export default function Register(){
 
         <form onSubmit={handleRegister} className="w-1/2 mx-auto space-y-6">
             <div>
-                <input type="text" name="" placeholder="Name" />
+                <input type="text" name="" placeholder="Name" value={FormData.name} onChange={(e) => setFormData({...FormData, name: e.target.value})}/>
             </div>
+
             <div>
-                <input type="text" name="" placeholder="Email" />
+                <input type="text" name="" placeholder="Email" value={FormData.email} onChange={(e) => setFormData({...FormData, email: e.target.value})}/>
             </div>
+
             <div>
-                <input type="password" name="" placeholder="Password" />
+                <input type="password" name="" placeholder="Password" value={FormData.password} onChange={(e) => setFormData({...FormData, password: e.target.value})}/>
             </div>
+
             <div>
-                <input type="password" name="" placeholder="Password Confirmation" />
+                <input type="password" name="" placeholder="Password Confirmation" value={FormData.password_confirmation} onChange={(e) => setFormData({...FormData, password_confirmation: e.target.value})} />
             </div>
-            <button className="primary-btn">Register</button>
+
+            <button type="submit" className="primary-btn">Register</button>
 
         </form>
         </>
